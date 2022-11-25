62c583ef0614adb216a0ebd1ca736f6409bc0ead
mihius03 <hieuminhbui68@gmail.com>
2022-11-25 17:19:03 +0700

    upload

---
+++ html/store/src/Data.js
@@ -0,0 +1,62 @@
+
+
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "jeans1",
+    price: 24.99,
+    desc: "ipsum dolor sit amet consectetur adipisicing...",
+    img: "img/jeans1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "jeans1",
+    price: 19.00,
+    desc: "ipsum dolor sit amet consectetur adipisicing...",
+    img: "img/jeans2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "jeans1",
+    price: 19.99,
+    desc: "ipsum dolor sit amet consectetur adipisicing...",
+    img: "img/jean3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "jeans1",
+    price: 20,
+    desc: "ipsum dolor sit amet consectetur adipisicing...",
+    img: "img/jean4.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "jeans1",
+    price: 20,
+    desc: "ipsum dolor sit amet consectetur adipisicing...",
+    img: "img/jean5.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "jeans1",
+    price: 14.09,
+    desc: "ipsum dolor sit amet consectetur adipisicing...",
+    img: "img/jean6.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "jeans1",
+    price: 15.05,
+    desc: "ipsum dolor sit amet consectetur adipisicing...",
+    img: "img/jean7.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "jeans1",
+    price: 24.99,
+    desc: "ipsum dolor sit amet consectetur adipisicing...",
+    img: "img/jean8.jpg",
+  },
+];
+
+
